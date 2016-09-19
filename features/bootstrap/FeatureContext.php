3dd7f3510ce901d7858b381f15ae31f619ae9e79
Becca85 <rebeccamaccio@gmail.com>
2016-09-19 13:40:02 +0200

    enregistrement des modification

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,22 +2,43 @@
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat \ Behat \ Exception \ PendingException ;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
 /**
  * Defines application features from the specific context.
  */
+class adresseDuSite {
+
+}
+
+
 class FeatureContext implements Context, SnippetAcceptingContext
 {
     /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
+     * @Given je tape l':arg1 du site
      */
-    public function __construct()
+    public function jeTapeLDuSite($arg1)
     {
+        $site = new adresseDuSite("URL");
     }
+
+    /**
+     * @When j'accède à la page :arg1
+     */
+    public function jAccedeALaPage($arg1)
+    {
+        $site = new adresseDuSite("URL");
+    }
+
+    /**
+     * @Then je vois l'ensemble des :arg1
+     */
+    public function jeVoisLEnsembleDes($arg1)
+    {
+        $site = new adresseDuSite("URL");
+    }
+
+
 }
---
+++ features/connect.feature
@@ -0,0 +1,14 @@
+# language: fr
+
+Fonctionnalité: Me connecter à la base de donnée
+    Afin que je puisse avoir la possibilité de me connecter à la base de donnée
+    Etant donné que je suis inscrit
+    Je dois être capable de me connecter
+
+
+Scénario: Visualisation de la page "Accueil"
+    Etant donné que je tape l'"URL" du site
+    Quand j'accède à la page "Accueil"
+    Alors je vois l'ensemble des "blocs"
+
+
